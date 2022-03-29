b73f3c4b5891b83e17621f0b85cb2b4264a125f9
blockchaindev0125 <blockchaindev0125@gmail.com>
2022-03-29 12:35:48 +0200

    added header and footer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './layout/header'
+import Footer from './layout/footer'
+import {
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+          <Routes>
+            <Header />
+            <Footer />
+          </Routes>
+      </Router>
     </div>
   );
 }
